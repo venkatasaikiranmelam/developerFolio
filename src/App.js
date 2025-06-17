ca9386e17fd268a38d05c375c91b5a49fc6b8827
venkatasaikiranmelam <114895412+venkatasaikiranmelam@users.noreply.github.com>
2025-06-16 22:01:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import { Analytics } from "@vercel/analytics/react"; // ✅ Import here
 
 function App() {
   return (
     <div>
       <Main />
+      <Analytics /> {/* ✅ Add this line just below Main */}
     </div>
   );
 }
